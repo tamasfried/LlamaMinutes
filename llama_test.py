878bfc2f39526f9603e7d215eaeeca24ab871a47
tamasfried <tomwfmorton@gmail.com>
2025-09-24 15:16:54 +0100

    deleted test file

--- llama_test.py
+++
@@ -1,8 +0,0 @@
-from langchain_ollama import OllamaLLM
-
-llm = OllamaLLM(model="llama3.2:1b")
-
-response = llm.invoke("Summarize the history of the Roman Empire in a single sentence.")
-
-print(response)
-
